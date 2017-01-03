a4f0cc7f4dd5cdb87389624c3f0232e4fca75d41
Karol Masuhr <karol@masuhr.pl>
2017-01-03 18:02:43 +0100

    Dodano informacje o pojeździe po kliknięciu w marker

--- js/model/Car.js
+++ js/model/Car.js
@@ -12,11 +12,12 @@ autoControl.car = {
             animation: google.maps.Animation.DROP,
             map: autoControl.map.services.map,
             CarId: car.CarId,
+            carInfo: car,
             icon: autoControl.map.data.markerImage
         });
 
         google.maps.event.addListener(newMarker, 'click', function () {
-            autoControl.map.services.infoWindow.setContent('Hello World');
+            autoControl.map.services.infoWindow.setContent(autoControl.car.renderInfoContent(this));
             autoControl.map.services.infoWindow.open(
                 autoControl.map.services.map,
                 newMarker
@@ -26,32 +27,19 @@ autoControl.car = {
         return newMarker;
     },
 
-    methods: {
-        /**
-         * @return {string}
-         */
-        InfoWinowContent: function () {
-            return '<div id="content">' +
-                '<div id="siteNotice">' +
-                '</div>' +
-                '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-                '<div id="bodyContent">' +
-                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                'sandstone rock formation in the southern part of the ' +
-                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-                'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-                'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-                'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-                'Aboriginal people of the area. It has many springs, waterholes, ' +
-                'rock caves and ancient paintings. Uluru is listed as a World ' +
-                'Heritage Site.</p>' +
-                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-                'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-                '(last visited June 22, 2009).</p>' +
-                '</div>' +
-                '</div>';
-
-        }
+    renderInfoContent: function (car) {
+        return '' +
+            '<div id="content">' +
+            '   <div id="siteNotice">' +
+            '   </div>' +
+            '   <h6 id="firstHeading" class="firstHeading">' +
+                    car.carInfo.Brand + ' ' + car.carInfo.Model + ' ' + car.carInfo.VIN +
+            '   </h6>' +
+            '   <div id="bodyContent">' +
+            '       <p>Prędkość: ' + car.carInfo.Speed + 'km/h</p>' +
+            '       <p>Obroty: ' + car.carInfo.Rpm + 'rpm</p>' +
+            '       <p>Obciążenie silnika: ' + car.carInfo.EngineLoad + '%</p>' +
+            '   </div>' +
+            '</div>';
     }
 };
