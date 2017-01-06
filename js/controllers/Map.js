4bd904403c965369ab8ba897050a93a05c1db2fd
kazurrr <Ka43ro21>
2017-01-06 20:11:00 +0100

    Wyznaczanie trasy z lokalizacji makrekra lub wpisanego adresu

--- js/controllers/Map.js
+++ js/controllers/Map.js
@@ -12,7 +12,7 @@ autoControl.map = {
     },
 
     services: {
-        route: null,
+        directionsService: null,
         directionDisplay: null,
         traffic: null,
         map: null,
--- js/controllers/Route.js
+++ js/controllers/Route.js
@@ -4,10 +4,19 @@ if (!autoControl.route)
 autoControl.route = {
     jqueryMap: {
         mainWrapper: null,
+        routeTextWrapper: null,
         fromWrapper: null,
         toWrapper: null,
         searchButton: null,
         clearButon: null
+    },
+
+    dictionary: {
+        originLabel: 'from',
+        originDescription: 'Początek trasy',
+
+        destinationLabel: 'to',
+        destinationDescription: 'Miejsce docelowe'
     },
 
     state: {
@@ -22,9 +31,9 @@ autoControl.route = {
         initModule: function (divID) {
             autoControl.route.jqueryMap.mainWrapper = $('#' + divID);
 
-            autoControl.map.services.route = new google.maps.DirectionsService();
+            autoControl.map.services.directionsService = new google.maps.DirectionsService();
             autoControl.map.services.directionDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
-            autoControl.map.services.directionDisplay.setPanel(document.getElementById('route-display-text'));
+
         },
 
         renderModule: function () {
@@ -34,6 +43,9 @@ autoControl.route = {
             autoControl.route.jqueryMap.toWrapper = $('#route-to-input-wrapper');
             autoControl.route.jqueryMap.searchButton = $('#route-search-button');
             autoControl.route.jqueryMap.clearButon = $('#route-clear-button');
+            autoControl.route.jqueryMap.routeTextWrapper = $('#route-direction-text');
+
+            autoControl.map.services.directionDisplay.setPanel(document.getElementById('route-direction-text'));
 
             autoControl.route.event.bindEvents();
         },
@@ -46,13 +58,14 @@ autoControl.route = {
                 '   <label for="route-from-marker">Marker</label>' +
 
                 '   <input name="route-from-marker-type" type="radio" id="route-from-search"/>' +
-                '   <label for="route-from-search">Wyszukaj</label>' +
+                '   <label for="route-from-search">Adres</label>' +
 
-                '   <input name="route-from-marker-type" type="radio" id="route-from-mapclick"/>' +
+                '   <input name="route-from-marker-type" type="radio" id="route-from-mapclick" disabled/>' +
                 '   <label for="route-from-mapclick">Mapa</label>' +
 
                 '   <div id="route-from-input-wrapper" class="route-input-wrapper input-field col s12">' +
-                autoControl.route.render.marker('from', 'Początek trasy') +
+                autoControl.route.render.marker(autoControl.route.dictionary.originLabel,
+                    autoControl.route.dictionary.originDescription) +
                 '   </div>' +
 
                 '</div>' +
@@ -62,13 +75,14 @@ autoControl.route = {
                 '   <label for="route-to-marker">Marker</label>' +
 
                 '   <input name="route-to-marker-type" type="radio" id="route-to-search"/>' +
-                '   <label for="route-to-search">Wyszukaj</label>' +
+                '   <label for="route-to-search">Adres</label>' +
 
-                '   <input name="route-to-marker-type" type="radio" id="route-to-mapclick"/>' +
+                '   <input name="route-to-marker-type" type="radio" id="route-to-mapclick" disabled/>' +
                 '   <label for="route-to-mapclick">Mapa</label>' +
 
                 '   <div id="route-to-input-wrapper" class="route-input-wrapper input-field col s12">' +
-                autoControl.route.render.marker('to', 'Miejsce docelowe') +
+                autoControl.route.render.marker(autoControl.route.dictionary.destinationLabel,
+                    autoControl.route.dictionary.destinationDescription) +
                 '   </div>' +
 
                 '</div>' +
@@ -76,51 +90,97 @@ autoControl.route = {
                 '   <button id="route-search-button" class="waves-effect waves-light btn small">Wyszukaj trasę</button>' +
                 '   <button id="route-clear-button" class="waves-effect waves-light btn small">Wyczyść trasę</button>' +
                 '</div>' +
-                '</form>';
-        },
+                '</form>' +
 
+                '<div id="route-direction-text">' +
+
+                '</div>';
+        },
 
         bindEvents: function () {
             $("input[name=route-from-marker-type]:radio").change(autoControl.route.event.changeSearchType);
             $("input[name=route-to-marker-type]:radio").change(autoControl.route.event.changeSearchType);
 
             autoControl.route.jqueryMap.searchButton.click(autoControl.route.event.getDirection);
+            autoControl.route.jqueryMap.clearButon.click(autoControl.route.event.clearDirection);
         },
 
         changeSearchType: function (data) {
-            var positionTo;
-            var positionDesc;
+            var positionLabel;
+            var positionDescription;
             var wrapper;
 
-            if (data.currentTarget.id.includes('to')) {
-                positionTo = 'to';
-                positionDesc = 'Miejsce docelowe';
+            console.log(data.currentTarget.id);
+
+            if (data.currentTarget.id.includes(autoControl.route.dictionary.destinationLabel)) {
+                positionLabel = autoControl.route.dictionary.destinationLabel;
+                positionDescription = autoControl.route.dictionary.destinationDescription;
                 wrapper = autoControl.route.jqueryMap.toWrapper[0];
             } else {
-                positionTo = 'from';
-                positionDesc = 'Początek trasy';
+                positionLabel = autoControl.route.dictionary.originLabel;
+                positionDescription = autoControl.route.dictionary.originDescription;
                 wrapper = autoControl.route.jqueryMap.fromWrapper[0];
             }
 
             if (data.currentTarget.id.includes('marker')) {
-                wrapper.innerHTML = autoControl.route.render.marker(positionTo, positionDesc);
+                wrapper.innerHTML = autoControl.route.render.marker(positionLabel, positionDescription);
             } else if (data.currentTarget.id.includes('search')) {
-                wrapper.innerHTML = autoControl.route.render.search(positionTo, positionDesc);
+                wrapper.innerHTML = autoControl.route.render.search(positionLabel, positionDescription);
             } else {
-                wrapper.innerHTML = autoControl.route.render.mapClick(positionTo, positionDesc);
+                wrapper.innerHTML = autoControl.route.render.mapClick(positionLabel, positionDescription);
             }
         },
 
         getDirection: function () {
-            console.log('');
+            var from = autoControl.route.event.getLatLon(autoControl.route.dictionary.originLabel);
+            var to = autoControl.route.event.getLatLon(autoControl.route.dictionary.destinationLabel);
+
+            if (from == null || to == null) {
+                autoControl.app.event.showToast('Podaj wszystkie punkty trasy');
+                return;
+            }
+
+            console.log(from);
+            console.log(to);
+
+            autoControl.map.services.directionDisplay.setMap(autoControl.map.services.map);
+
+            autoControl.map.services.directionsService.route({
+                    origin: from,
+                    destination: to,
+                    travelMode: 'DRIVING'
+                },
+                function (response, status) {
+                    if (status === 'OK') {
+                        autoControl.map.services.directionDisplay.setDirections(response);
+                    } else {
+                        console.log('Directions request failed due to ' + status);
+                    }
+                });
         },
 
         clearDirection: function () {
+            autoControl.map.services.directionDisplay.setMap(null);
+            autoControl.route.jqueryMap.routeTextWrapper.html('');
+        },
 
+        getLatLon: function (position) {
+            var currentType = $('input[name=route-' + position + '-marker-type]:checked');
+            var inputValue = $('#route-' + position + '-input').val();
+
+            if (currentType.attr('id').includes('marker')) {
+                if (inputValue == null)
+                    return null;
+
+                return autoControl.map.carsQuery.getMarkerByCarId(inputValue).getPosition();
+            } else if (currentType.attr('id').includes('search')) {
+                //ToDo address validation
+                return inputValue
+            } else {
+                console.log(3);
+            }
         }
     },
-
-
 
     render: {
         marker: function (place, label) {
@@ -143,7 +203,10 @@ autoControl.route = {
         },
 
         search: function (place, label) {
-            return 'Search';
+            return '<div class="input-field">' +
+                '   <input id="route-' + place + '-input" type="text" class="validate">' +
+                '   <label for="route-' + place + '-input">' + label + '</label>' +
+                '</div>';
         },
 
         mapClick: function (place, label) {
